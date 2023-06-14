dd9bd684dc51d8273be6ed5b7ac555c796d87351
Rutuja <rodagerutuja15@gmail.com>
2023-06-13 18:00:43 -0700

    Add existing projects files to Git

---
+++ client/src/Component/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+export default function Home() {
+  return (
+    <section>      
+      <div className="container">
+        <div className="row ">
+            
+
+        </div>
+      </div>
+  </section>
+
+  )
+}
